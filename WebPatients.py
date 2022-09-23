d9953712a2174961995b48658fda19dfe15fd82f
santoshraj4u <santoshraj4u@gmail.com>
2022-09-23 07:41:18 +0000

    commit 1 23 Sep 2022

---
+++ WebPatients.py
@@ -0,0 +1,132 @@
+from flask import Flask, request, jsonify, render_template
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from http import HTTPStatus
+
+app = Flask(__name__)
+api = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost/hospitaldb'
+db = SQLAlchemy(app)
+
+class Patient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # this is the primary key
+    name = db.Column(db.String(80), nullable=False)
+    phonenumber = db.Column(db.BigInteger(), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    bedtype = db.Column(db.String(80), nullable=False)
+    address = db.Column(db.String(80), nullable=False)
+    state = db.Column(db.String(80), nullable=False)
+    city = db.Column(db.String(80), nullable=False)
+    patientstatus = db.Column(db.String(80), nullable=False)
+
+    @staticmethod
+    def add_patient(name, phonenumber, age, bedtype, address, state, city, patientstatus):
+        new = Patient(name = name, phonenumber = phonenumber,
+                      age = age, bedtype = bedtype, address = address,
+                      state = state, city = city, patientstatus = patientstatus)
+        db.session.add(new)
+        db.session.commit()
+
+    @staticmethod
+    def get_patient():
+        return Patient.query.all()
+
+    @staticmethod
+    def get_patient2(phonenumber):
+        return Patient.query.filter_by(phonenumber=phonenumber).first()
+
+    @staticmethod
+    def delete_patient_by_id(phonenumber):
+        mv=Patient.query.filter_by(phonenumber=phonenumber).delete()
+        db.session.commit()
+        return(mv)
+
+    @staticmethod
+    def update_Patient_by_id(id, name, phonenumber, age, bedtype, address, state, city, patientstatus):
+        mv = Patient.query.filter_by(phonenumber=phonenumber).first()
+        if mv:
+            mv.name = name
+            mv.phonenumber = phonenumber
+            mv.age = age
+            mv.bedtype = bedtype
+            mv.address = address
+            mv.state = state
+            mv.city = city
+            mv.patientstatus = patientstatus
+            db.session.commit()
+            return (mv)
+        else:
+            print("No Patient with this phone num")
+            return None
+
+
+#------------------------------------------- Resources ------------------------------------------------------#
+
+# Default page -- Home
+@app.route("/")
+def homepage():
+    return render_template("home.html")
+
+
+
+# Selecting all data
+@app.route("/getAllPatients", methods = ['GET'])
+def getAllPatients():
+    data = Patient.get_patient()
+    return render_template("AllPatients.html", data=data)
+
+
+# Getting patient data from phonenumber
+@app.route("/get_patient_by_id", methods = ['GET'])
+def get_patient_by_id():
+    dd = request.get_json()
+    data = Patient.get_patient2(dd.phonenumber)
+    print(data)
+    res=[]
+    if data != None:
+        res.append({"name":data.name, "phonenumber":data.phonenumber,
+                    "age": data.age, "bedtype": data.bedtype, "address": data.address,
+                    "state": data.state, "city": data.city, "patientstatus": data.patientstatus})
+        return(jsonify(res))
+    else:
+        return jsonify({'message':'Not found', 'status':HTTPStatus.NOT_FOUND})
+
+
+# Inserting data
+@app.route("/register_patient", methods = ['POST'])
+def register_patient():
+    data = request.get_json()
+    print(data)
+    Patient.add_patient(name = data["name"], phonenumber = data["phonenumber"],
+                        age = data["age"], bedtype = data["bedtype"], address = data["address"],
+                        state = data["state"], city = data["city"], patientstatus = data["patientstatus"])
+    return jsonify({'message':'Patient Registered', 'status':HTTPStatus.OK})
+
+# Updating data
+@app.route("/edit_patient", methods = ['PUT'])
+def edit_patient():
+    data = request.get_json()
+    result = Patient.update_Patient_by_id(id,data["name"], data["phonenumber"],
+                                 data["age"],data["bedtype"],data["address"],
+                                 data["state"],data["city"],data["patientstatus"])
+    if result:
+        return jsonify({'message': 'Updated', 'status': HTTPStatus.OK})
+    else:
+        return jsonify({'message':'Not found', 'status':HTTPStatus.NOT_FOUND})
+
+
+# Deleting data
+@app.route("/delete_patient", methods = ['DELETE'])
+def delete_patient():
+    dd = request.get_json()
+    data = Patient.delete_patient_by_id(dd['phonenumber'])
+    if data:
+        return jsonify({'message':'Deleted', 'status':HTTPStatus.OK})
+    else:
+        return jsonify({'message':'Not found', 'status':HTTPStatus.NOT_FOUND})
+
+
+
+if __name__ == "__main__":
+    app.run()
